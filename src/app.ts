6f6c0beffb955a8ecce7c417c75664e9f164b281
Pepo <pepo@uprtcl.io>
2021-09-30 10:58:04 +0200

    create notes working

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,111 @@
-import { html, css, LitElement } from 'lit-element';
+import { html, css, LitElement, internalProperty, query } from 'lit-element';
 
-export class App extends LitElement {
-  async firstUpdated() {}
+import { servicesConnect } from '@uprtcl/evees-ui';
+import { Entity } from '@uprtcl/evees';
+
+interface Note {
+  text: string;
+}
+
+/** In LitElement we uses the `servicesConnect` function to inject
+ * the evees service into a web-component at run-time. */
+export class App extends servicesConnect(LitElement) {
+  @internalProperty()
+  notes: Map<string, Entity<Note>> = new Map();
+
+  @query('#new-note')
+  textearea: HTMLTextAreaElement;
+
+  async firstUpdated() {
+    console.log(`The evees service is injected under this.evees`, {
+      evees: this.evees,
+    });
+
+    /** load all notes */
+    await this.loadNotes();
+  }
+
+  async loadNotes() {
+    /** explore without input parameters will return all known perspectives/notes */
+    const result = await this.evees.explore({});
+    await Promise.all(
+      result.perspectiveIds.map(async (noteId) => {
+        const note = await this.evees.getPerspectiveData<Note>(noteId);
+        this.notes.set(noteId, note);
+      })
+    );
+  }
+
+  async createNote() {
+    const text = this.textearea.value;
+    const noteId = await this.evees.createEvee({ object: { text } });
+    const note = await this.evees.getPerspectiveData<Note>(noteId);
+    this.notes = this.notes.set(noteId, note);
+
+    this.textearea.value = '';
+    this.textearea.focus();
+
+    // re-render
+    this.requestUpdate();
+  }
+
+  async updateNote(noteId: string, text: string) {
+    await this.evees.updatePerspectiveData({
+      perspectiveId: noteId,
+      object: { text },
+    });
+
+    // re-render
+    this.requestUpdate();
+  }
 
   render() {
-    return html`<div id="app"><h1>Hello Evees</h1></div> `;
+    return html`<div id="app">
+      <h1>Hello Evees</h1>
+      <textarea id="new-note"></textarea
+      ><button @click=${() => this.createNote()}>create</button>
+      ${Array.from(this.notes.keys()).map(
+        (noteId) =>
+          html`<div class="card">${this.notes.get(noteId).object.text}</div>`
+      )}
+    </div> `;
   }
 
   static get styles() {
     return [
       css`
-        :host {
-          height: 100%;
-          flex-direction: column;
-          display: flex;
-          justify-content: center;
+        #app {
+          padding: 10vw 10vw;
+          max-width: 600px;
         }
 
-        #app {
-          height: 100%;
-          display: flex;
-          flex-direction: column;
-          overflow: auto;
+        textarea {
+          resize: vertical;
+          padding: 2vw;
+          width: calc(100% - 4vw);
+        }
+
+        button {
+          width: 100%;
+          margin-top: 2vw;
+          margin-bottom: 6vw;
+          padding: 1vw;
+          background-color: #405886;
+          border: none;
+          border-radius: 6px;
+          cursor: pointer;
+          color: white;
+        }
+
+        button:hover {
+          background-color: #203152;
+        }
+
+        .card {
+          padding: 2vw;
+          border: 1px solid;
+          border-radius: 5px;
+          margin-bottom: 2vw;
         }
       `,
     ];
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
+import {
+  ClientRemoteLocal,
+  EntityRemoteLocal,
+  EntityResolverBase,
+  LocalExplore,
+  PerspectivesStoreDB,
+  RouterEntityResolver,
+} from '@uprtcl/evees';
+import { initDefault, MultiContainer } from '@uprtcl/evees-ui';
 import { App } from './app';
 
 (async function () {
+  console.log('Initializing App');
+
+  /** The goal is to initialize the EveesService and store it into app-container element. */
+
+  /** We first need to instantiate an EntityResolver, which will resolve hashes into their
+   * corresponding entities */
+  const entityRemote = new EntityRemoteLocal();
+  const entityRouter = new RouterEntityResolver([entityRemote]);
+  const entityResolver = new EntityResolverBase(entityRouter);
+
+  /** We then instantiate a PerspectiveStoreDB using IndexedDB to serve as our "local" Remote */
+  const perspectiveStoreDB = new PerspectivesStoreDB();
+
+  /** The local explore service receives the local PerspectiveStoreDB to query for perspectives */
+  const exploreService = new LocalExplore(perspectiveStoreDB);
+
+  /** The EveesRemote is a local remote that needs the EntityResolver, to resolve entities during
+   * its operations, the perspectiveStoreDB, to create and update perspectives, the entityRemote,
+   * to persist entities, and the ExploreService, to query perspectives */
+  const eveesRemote = new ClientRemoteLocal(
+    entityResolver,
+    perspectiveStoreDB,
+    entityRemote,
+    exploreService
+  );
+
+  /** the initDefault function build the stack of Clients, with a top-layer Client that
+   * mutations in-memory, a persistent temporary layer that stores mutations locally in IndexedDB,
+   * and the EveesRemotes, which in this case is also storing evees heads in IndexedDB.
+   *
+   * When we replace the EveesRemote with a real remote, like a web server, the intermediate
+   * persisten layer will provide local-first functionalities.
+   */
+  const evees = initDefault([eveesRemote], entityResolver);
+
+  customElements.define('app-container', MultiContainer(evees));
+
   customElements.define('uprtcl-sample-app', App);
 })();
